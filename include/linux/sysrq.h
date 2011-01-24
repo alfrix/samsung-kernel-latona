bd40c6fa33539478a90ead589f8e47aaaf22ce66
Andy Whitcroft <apw@canonical.com>
2011-01-24 09:31:38 -0800

    Input: sysrq - ensure sysrq_enabled and __sysrq_enabled are consistent
    Currently sysrq_enabled and __sysrq_enabled are initialised separately
    and inconsistently, leading to sysrq being actually enabled by reported
    as not enabled in sysfs.  The first change to the sysfs configurable
    synchronises these two:
    
        static int __read_mostly sysrq_enabled = 1;
        static int __sysrq_enabled;
    
    Add a common define to carry the default for these preventing them becoming
    out of sync again.  Default this to 1 to mirror previous behaviour.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Cc: stable@kernel.org
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- include/linux/sysrq.h
+++ include/linux/sysrq.h
@@ -19,6 +19,9 @@
 struct pt_regs;
 struct tty_struct;
 
+/* Enable/disable SYSRQ support by default (0==no, 1==yes). */
+#define SYSRQ_DEFAULT_ENABLE	1
+
 /* Possible values of bitmask for enabling sysrq functions */
 /* 0x0001 is reserved for enable everything */
 #define SYSRQ_ENABLE_LOG	0x0002
